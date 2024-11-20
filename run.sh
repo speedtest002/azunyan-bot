c0f4538e60bc5337940e4f317f54dfe09ed1e0dc
peashooter <son061203@gmail.com>
2024-11-20 15:43:36 +0700

    Auto deploy when push to main

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
-#chmox +x run.sh
+#!/bin/bash
+source .env
+find . -type f ! -name ".env" ! -name "run.sh" -delete
+
+# Pull code from GitHub
+git clone https://$GH_ACCESS_TOKEN@github.com/$GH_USERNAME/$GH_REPO.git temp_dir
+mv temp_dir/* temp_dir/.* .  # Di chuyển cả file ẩn
+rm -rf temp_dir
+rm -rf .git
+
 pip install -r requirements.txt
 python bot.py
