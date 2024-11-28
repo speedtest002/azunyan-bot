5b46996636f81e9e35edf937e150dbfbb2600e6b
peashooter <son061203@gmail.com>
2024-11-28 16:13:55 +0700

    upload user_qr.json

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# azunyan-bot
+# azunyan discord bot
+A simple bot based on discord.py with automatic push to a free discord bot hosting service [sillydevelop](https://panel.sillydevelopment.co.uk/)
+
+[Invite azunyan to your server](https://discord.com/oauth2/authorize?client_id=1229243192478662697)
+
+Self-host?
+
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source .env
-find . -mindepth 1 ! -name ".env" ! -name "run.sh" -exec rm -rf {} +
-
+#find . -mindepth 1 ! -name ".env" ! -name "run.sh" -exec rm -rf {} +
 # Pull code from GitHub
 git clone https://$GH_ACCESS_TOKEN@github.com/$GH_USERNAME/$GH_REPO.git temp_dir
 mv temp_dir/* temp_dir/.* .  # Di chuyển cả file ẩn
