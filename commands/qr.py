1a920f9ee6eb8228c9cb01e16e962299009aca8b
peashooter <son061203@gmail.com>
2024-12-20 22:37:29 +0700

    update: qr command

--- commands/qr.py
+++ commands/qr.py
@@ -1,80 +1,136 @@
 from discord.ext import commands
 from discord import *
 from feature import MongoManager
+import re
+import os
 
 class QRCodeCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self._last_member = None
         self.qr_collection = MongoManager.get_collection("qr")
+        self.banks_collection = MongoManager.get_collection("banks")
+        self.bank_aliases = self.load_banks()
 
-    @commands.hybrid_command(name = "qr_ngân_hàng", aliases = ['qr', 'bank'], with_app_command=True)
-    @app_commands.allowed_installs(guilds=True, users=True)
-    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-    async def qr_ngân_hàng(
-        self,
-        ctx: commands.Context,
-        số_tài_khoản: str = None,
-        ngân_hàng: str = None,
-        số_tiền: str = None, 
-        nội_dung: str = None,
-        chủ_tài_khoản: str = None
-    ):
-        """
-        Tạo mã QR từ số tài khoản ngân hàng
-
-        Parameters
-        ----------
-        số_tài_khoản: str,
-            Số tài khoản người nhận
-        ngân_hàng: str
-            Tên ngân hàng
-        số_tiền: str = None, 
-            Số tiền nhận
-        nội_dung: str = None,
-            Nội dung chuyển khoản
-        chủ_tài_khoản: str = None
-            Tên chủ tài khoản
-        """
-        if số_tài_khoản is not None and ngân_hàng is None:
-            await ctx.message.delete()
-            await ctx.send("Vui lòng nhập tên ngân hàng (ví dụ: vcb hoặc vietcombank).", ephemeral=True, delete_after=5)
-            return
-        
-        user_id = ctx.author.id
+    def qr_bank_core(self, user_id, bank_number, bank_name, amount, add_info: str = None, account_name: str = None) -> tuple[bool, str]:
+        if bank_number is not None and bank_name is None:
+            return False, f"Vui lòng nhập tên ngân hàng (ví dụ: vcb hoặc vietcombank hoặc dùng lệnh {os.getenv("PREFIX")}qr --help/-h để xem hướng dẫn)."
+            
         user_data = self.qr_collection.find_one({"_id": user_id})
         if user_data is None:
-            if số_tài_khoản is None and ngân_hàng is None:
-                await ctx.send("Bạn cần dùng lệnh này lần đầu với đúng cú pháp để lưu thông tin QR!", delete_after=5)
-                return
+            if bank_number is None and bank_name is None:
+                return False, "Bạn cần dùng lệnh này lần đầu với đúng cú pháp để lưu thông tin QR!"
             user_data = {
-            "_id": user_id,
-            "number": số_tài_khoản,
-            "name": ngân_hàng
+                "_id": user_id,
+                "number": bank_number,
+                "name": bank_name
             }
             self.qr_collection.insert_one(user_data)
         else:
-            if số_tài_khoản is not None and ngân_hàng is not None:  # Có thông tin mới để cập nhật
+            if bank_number is not None and bank_name is not None:  # Có thông tin mới để cập nhật
                 updated_data = {
-                    "number": số_tài_khoản,
-                    "name": ngân_hàng
+                    "number": bank_number,
+                    "name": bank_name
                 }
                 self.qr_collection.update_one({"_id": user_id}, {"$set": updated_data})
                 user_data.update(updated_data)
             else:
-                số_tài_khoản = user_data["number"]
-                ngân_hàng = user_data["name"]
+                bank_number = user_data["number"]
+                bank_name = user_data["name"]
         
         
-        url = f"https://img.vietqr.io/image/{ngân_hàng}-{số_tài_khoản}-print.png?"
-        if số_tiền is not None:
-            url += f"amount={số_tiền}"
-        if nội_dung is not None:
-            nội_dung = nội_dung.replace(" ", "%20")
-            url += f"&addInfo={nội_dung}"
-        if chủ_tài_khoản is not None:
-            chủ_tài_khoản = chủ_tài_khoản.replace(" ", "%20")
-            url += f"&accountName={chủ_tài_khoản}"
-        await ctx.send(url)
-    
+        url = f"https://img.vietqr.io/image/{bank_name}-{bank_number}-print.png?"
+        if amount is not None:
+            url += f"amount={amount}"
+        if add_info is not None:
+            add_info = add_info.replace(" ", "%20")
+            url += f"&addInfo={add_info}"
+        if account_name is not None:
+            account_name = account_name.replace(" ", "%20")
+            url += f"&accountName={account_name}"
+        return True, url
+
+    # slash command
+    @app_commands.command(name = "qr_ngân_hàng", description="Tạo mã QR từ số tài khoản ngân hàng", with_app_command=True)
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    @app_commands.rename(bank_number = "số_tài_khoản", bank_name = "ngân_hàng", amount = "số_tiền", add_info = "nội_dung", account_name = "chủ_tài_khoản")
+    @app_commands.describe(bank_number = "Số tài khoản người nhận", bank_name = "Tên ngân hàng", amount = "Số tiền nhận", add_info = "Nội dung chuyển khoản", account_name = "Tên chủ tài khoản")
+    async def qr_bank_slash(
+        self,
+        ctx: commands.Context,
+        bank_number: str,
+        bank_name: str,
+        amount: str = None, 
+        add_info: str = None,
+        account_name: str = None
+    ):
+        user_id = ctx.author.id
+        state, message = self.qr_bank_core(user_id, bank_number, bank_name, amount, add_info, account_name)
+        if state is False:
+            await ctx.send(message, ephemeral=True)
+            return
+        await ctx.send(message)
+
+    # prefix qr
+    #azuqr -b vcb -n 123456789 -a 100000 -d "Chuyển tiền" -c
+    @commands.command(name="qr_ngân_hàng", aliases=["qr", "bank"])
+    async def qr_bank_slash(
+        self,
+        ctx,
+        *args
+    ):
+        user_id = ctx.author.id
+        
+        #state, message = self.qr_bank_core(user_id, bank_number, bank_name, amount, add_info, account_name)
+        #if state is False:
+        #    await ctx.send(message, ephemeral=True)
+        #    return
+        await ctx.send(message)
+
+    ### zalo qr clone
+    def load_banks(self):
+        bank_data = {}
+        try:
+            banks = self.banks_collection.find()
+            for bank in banks:
+                for alias in bank["aliases"]:
+                    bank_data[alias] = bank["name"]
+        except Exception as e:
+            print(f"Không thể tải dữ liệu ngân hàng từ MongoDB: {e}")
+        return bank_data
+
+
+    def generate_qr_code(self, stk, bank_name):
+        return f"https://img.vietqr.io/image/{bank_name}-{stk}-print.png"
+        #return f"QR Code cho STK {stk} tại {bank_name}"
+
+    @commands.Cog.listener("on_message")
+    async def send_qr(self, message):
+        if message.author.bot:
+            return
+
+        content = message.content.lower()
+
+        stk_match = re.search(r'\b\d{6,19}\b', content) # theo VietQR Số tài khoản người nhận quy ước bao gồm chữ hoặc số, tối đa 19 kí tự.
+        stk = stk_match.group() if stk_match else None
+
+        if not stk:
+            return
+
+        words = content.split()
+
+        bank_name = None
+        for word in words:
+            if word in self.bank_aliases:
+                bank_name = self.bank_aliases[word]
+                break
+
+        if not bank_name:
+            return
+
+        qr_code = self.generate_qr_code(stk, bank_name)
+        await message.channel.send(f"{qr_code}")
+
 async def setup(bot):
     await bot.add_cog(QRCodeCommand(bot))
