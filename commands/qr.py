8ed7168d16997bb4649e42e22799db0d2897b17b
peashooter <son061203@gmail.com>
2024-12-23 23:37:40 +0700

    check point: prefix QR todo: - debug option command - add vietqr api

--- commands/qr.py
+++ commands/qr.py
@@ -3,6 +3,8 @@ from discord import *
 from feature import MongoManager
 import re
 import os
+import click
+from click.testing import CliRunner
 
 class QRCodeCommand(commands.Cog):
     def __init__(self, bot):
@@ -11,62 +13,95 @@ class QRCodeCommand(commands.Cog):
         self.qr_collection = MongoManager.get_collection("qr")
         self.banks_collection = MongoManager.get_collection("banks")
         self.bank_aliases = self.load_banks()
+        self.runner = CliRunner()
+        self.cli_command = self._define_command()
 
-    def qr_bank_core(self, user_id, bank_number, bank_name, amount, add_info: str = None, account_name: str = None) -> tuple[bool, str]:
-        if bank_number is not None and bank_name is None:
-            return False, f"Vui lòng nhập tên ngân hàng (ví dụ: vcb hoặc vietcombank hoặc dùng lệnh {os.getenv("PREFIX")}qr --help/-h để xem hướng dẫn)."
-            
+    def find_information(self, user_id) -> bool: #kiem tra xem user_id da co thong tin chua
         user_data = self.qr_collection.find_one({"_id": user_id})
         if user_data is None:
-            if bank_number is None and bank_name is None:
-                return False, "Bạn cần dùng lệnh này lần đầu với đúng cú pháp để lưu thông tin QR!"
-            user_data = {
-                "_id": user_id,
-                "number": bank_number,
-                "name": bank_name
-            }
+            return False
+        return True
+
+    def insert_information(self, user_id, bank_number, bank_name):
+        user_data = {
+            "_id": user_id,
+            "number": bank_number,
+            "name": bank_name
+        }
+        try:
             self.qr_collection.insert_one(user_data)
-        else:
-            if bank_number is not None and bank_name is not None:  # Có thông tin mới để cập nhật
-                updated_data = {
-                    "number": bank_number,
-                    "name": bank_name
-                }
-                self.qr_collection.update_one({"_id": user_id}, {"$set": updated_data})
-                user_data.update(updated_data)
-            else:
-                bank_number = user_data["number"]
-                bank_name = user_data["name"]
-        
-        
+            return True
+        except Exception as e:
+            print(f"Failed to insert information for user {user_id}: {e}")
+            return False
+
+    def update_information(self, user_id, user_data, bank_number, bank_name):
+        updated_data = {
+            "number": bank_number,
+            "name": bank_name
+        }
+        try:
+            self.qr_collection.update_one({"_id": user_id}, {"$set": updated_data})
+            user_data.update(updated_data)
+            print(f"Update information for user {user_id} successfully.")
+            #return True
+        except Exception as e:
+            print(f"Failed to update information for user {user_id}: {e}")
+            #return False
+
+    def qr_generate(self, bank_number: str, bank_name: str, amount: str = None, description: str = None, account_name: str = None) -> str:
         url = f"https://img.vietqr.io/image/{bank_name}-{bank_number}-print.png?"
         if amount is not None:
             url += f"amount={amount}"
-        if add_info is not None:
-            add_info = add_info.replace(" ", "%20")
-            url += f"&addInfo={add_info}"
+        if description is not None:
+            description = description.replace(" ", "%20")
+            url += f"&addInfo={description}"
         if account_name is not None:
             account_name = account_name.replace(" ", "%20")
             url += f"&accountName={account_name}"
+        return url
+
+    def qr_bank_core(self, user_id: str, bank_number: str = None, bank_name: str = None, amount: str = None, description: str = None, account_name: str = None) -> tuple[bool, str]:
+        
+        #if bank_number is not None and bank_name is None:
+        #    return False, f"Vui lòng nhập tên ngân hàng (ví dụ: vcb hoặc vietcombank hoặc dùng lệnh {os.getenv("PREFIX")}qr --help/-h để xem hướng dẫn)."
+            
+        user_data = self.qr_collection.find_one({"_id": user_id})
+
+        if self.find_information(user_id) is False: # for Prefix only
+            if bank_number is None or bank_name is None:
+                return False, f"Bạn cần dùng lệnh này lần đầu với đầy đủ thông tin `stk và ngân hàng` để lưu thông tin QR! (dùng `{os.getenv('PREFIX')}qr --help/-h` để xem hướng dẫn)"
+            else:
+                self.insert_information(user_id, bank_number, bank_name)
+
+        else: # for both Slash and Prefix
+            if bank_number is not None and bank_name is not None: # Có thông tin mới để cập nhật
+                if (user_data["number"] == bank_number and user_data["name"] == bank_name) is False:
+                    self.update_information(user_id, user_data, bank_number, bank_name)
+            else: # Không có thông tin mới để cập nhật
+                bank_number = user_data["number"]
+                bank_name = user_data["name"]
+        
+        url = self.qr_generate(bank_number, bank_name, amount, description, account_name)
         return True, url
 
     # slash command
-    @app_commands.command(name = "qr_ngân_hàng", description="Tạo mã QR từ số tài khoản ngân hàng", with_app_command=True)
+    @app_commands.command(name = "qr_ngân_hàng", description="Tạo mã QR từ số tài khoản ngân hàng")
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-    @app_commands.rename(bank_number = "số_tài_khoản", bank_name = "ngân_hàng", amount = "số_tiền", add_info = "nội_dung", account_name = "chủ_tài_khoản")
-    @app_commands.describe(bank_number = "Số tài khoản người nhận", bank_name = "Tên ngân hàng", amount = "Số tiền nhận", add_info = "Nội dung chuyển khoản", account_name = "Tên chủ tài khoản")
+    @app_commands.rename(bank_number = "số_tài_khoản", bank_name = "ngân_hàng", amount = "số_tiền", description = "nội_dung", account_name = "chủ_tài_khoản")
+    @app_commands.describe(bank_number = "Số tài khoản người nhận", bank_name = "Tên ngân hàng", amount = "Số tiền nhận", description = "Nội dung chuyển khoản", account_name = "Tên chủ tài khoản")
     async def qr_bank_slash(
         self,
         ctx: commands.Context,
         bank_number: str,
         bank_name: str,
         amount: str = None, 
-        add_info: str = None,
+        description: str = None,
         account_name: str = None
     ):
         user_id = ctx.author.id
-        state, message = self.qr_bank_core(user_id, bank_number, bank_name, amount, add_info, account_name)
+        state, message = self.qr_bank_core(user_id, bank_number, bank_name, amount, description, account_name)
         if state is False:
             await ctx.send(message, ephemeral=True)
             return
@@ -74,19 +109,41 @@ class QRCodeCommand(commands.Cog):
 
     # prefix qr
     #azuqr -b vcb -n 123456789 -a 100000 -d "Chuyển tiền" -c
-    @commands.command(name="qr_ngân_hàng", aliases=["qr", "bank"])
-    async def qr_bank_slash(
-        self,
-        ctx,
-        *args
-    ):
-        user_id = ctx.author.id
-        
-        #state, message = self.qr_bank_core(user_id, bank_number, bank_name, amount, add_info, account_name)
-        #if state is False:
-        #    await ctx.send(message, ephemeral=True)
-        #    return
-        await ctx.send(message)
+    def _define_command(self):
+        @click.command()
+        @click.option("-b", default=None, help="Ngân hàng")
+        @click.option("-n", default=None, help="Số tài khoản")
+        @click.option("-a", default=None, help="Số tiền")
+        @click.option("-d", default=None, help="Nội dung")
+        @click.option("-c", is_flag=True, help="Flag check stk")
+        def command(b, n, a, d, c):
+            click.echo((b, n, a, d, c))  # In ra tuple để đọc từ output
+            return b, n, a, d, c
+
+        return command
+    def parse(self, args):
+        try:
+            result = self.runner.invoke(self.cli_command, args)
+
+            if result.exit_code != 0:
+                raise click.exceptions.ClickException(f"Lỗi: {result.output}")
+
+            # Đọc kết quả từ output (tuple dưới dạng chuỗi)
+            output = result.output.strip()
+            return eval(output)  # Convert chuỗi thành tuple
+        except click.exceptions.ClickException as e:
+            print(e)
+            return None
+    @commands.command(name="qr", aliases=["bank"])
+    async def qr_bank_prefix(self, ctx, *args):
+        try:
+            param = self.parse(args)
+            user_id = ctx.author.id
+            state, message = self.qr_bank_core(user_id, param[1], param[0], param[2], param[3])
+            await ctx.send(message, ephemeral=state)
+        except Exception as e:
+            await ctx.send(f"Cú pháp lệnh không hợp lệ, dùng lệnh `{os.getenv('PREFIX')}qr -h` để xem hướng dẫn ", ephemeral=True)
+
 
     ### zalo qr clone
     def load_banks(self):
@@ -100,19 +157,15 @@ class QRCodeCommand(commands.Cog):
             print(f"Không thể tải dữ liệu ngân hàng từ MongoDB: {e}")
         return bank_data
 
-
-    def generate_qr_code(self, stk, bank_name):
-        return f"https://img.vietqr.io/image/{bank_name}-{stk}-print.png"
-        #return f"QR Code cho STK {stk} tại {bank_name}"
-
     @commands.Cog.listener("on_message")
     async def send_qr(self, message):
         if message.author.bot:
             return
-
+        if any(message.content.startswith(prefix) for prefix in await self.bot.get_prefix(message)):
+            return
         content = message.content.lower()
 
-        stk_match = re.search(r'\b\d{6,19}\b', content) # theo VietQR Số tài khoản người nhận quy ước bao gồm chữ hoặc số, tối đa 19 kí tự.
+        stk_match = re.search(r'\b\d{6,19}\b', content) # theo VietQR Số tài khoản người nhận quy ước bao gồm chữ hoặc số, tối đa 19 kí tự. Here I use number only
         stk = stk_match.group() if stk_match else None
 
         if not stk:
@@ -129,7 +182,7 @@ class QRCodeCommand(commands.Cog):
         if not bank_name:
             return
 
-        qr_code = self.generate_qr_code(stk, bank_name)
+        qr_code = self.qr_generate(stk, bank_name)
         await message.channel.send(f"{qr_code}")
 
 async def setup(bot):
