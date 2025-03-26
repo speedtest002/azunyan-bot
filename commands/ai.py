c64201d6c8307c588902d31b956b049147118f44
Son Pham <peashooter@lawn.io.vn>
2025-03-27 00:32:48 +0700

    fix: Khắc phục một số vấn đề liên quan tới thư viện discord.py và cho phép người dùng tự nhập model

--- commands/ai.py
+++ commands/ai.py
@@ -14,33 +14,28 @@ class AIChatCommand(commands.Cog):
         self.AIChat_api_key_collection = MongoManager.get_collection('AIChat_api_keys')
         
     models =[
-        app_commands.Choice(name='Nên chọn cái nào?', value="help"),
-        app_commands.Choice(name='deepseek-r1', value='deepseek/deepseek-r1'),
-        app_commands.Choice(name='deepseek-chat', value='deepseek/deepseek-chat'),
-        app_commands.Choice(name='deepseek-r1-distill-llama-70b', value='deepseek/deepseek-r1-distill-llama-70b'),
-        app_commands.Choice(name='google-gemini-2.0-flash-thinking-exp', value='google/gemini-2.0-flash-thinking-exp'),
-        app_commands.Choice(name='google-gemini-2.0-flash-lite-preview-02-05', value='google/gemini-2.0-flash-lite-preview-02-05'),
-        app_commands.Choice(name='qwen-vl-plus', value='qwen/qwen-vl-plus')
-    ] 
+        'deepseek/deepseek-r1',
+        'google/gemini-2.0-flash-lite-preview-02-05',
+        'google/gemini-2.5-pro-exp-03-25',
+        'qwen/qwen2.5-vl-32b-instruct'
+    ]
+    async def model_autocomplete(
+        self,
+        interaction: Interaction,
+        current: str,
+    ) -> List[app_commands.Choice[str]]:
+        return [
+            app_commands.Choice(name=model, value=model)
+            for model in AIChatCommand.models if current.lower() in model.lower()
+        ]
+    @app_commands.autocomplete(model=model_autocomplete)
     @app_commands.command(name="ai_setting", description="Lựa chọn mô hình và thêm api key.")
-    @app_commands.choices(model=models)
     @app_commands.describe(
-        model='Mô hình',
+        model='Mô hình, có thể tìm thêm từ https://openrouter.ai/models?q=%28free%29',
         api_key='Cách lấy: Tạo tài khoản ở https://openrouter.ai/ -> Keys -> Create Key.'
     )
     @app_commands.dm_only()
     async def ai_setting(self, interaction: Interaction, model: str, api_key: str = None):
-        if model == "help":
-            response = [
-                '`deepseek-r1` - Mô hình tàu khựa, rất chậm',
-                '`deepseek-chat` - Cũng mô hình tàu tàu khựa nhưng ngu hơn, rất chậm',
-                '`deepseek-r1-distill-llama-70b` - Cũng mô hình tàu tàu khựa nhưng nhanh hơn và ngu hơn',
-                '`google-gemini-2.0-flash-thinking-exp` - Mô hình của bố Mẽo, nhanh, biết suy nghĩ và hỗ trợ đọc link hình ảnh',
-                '`google-gemini-2.0-flash-lite-preview-02-05` - Cũng là mô hình của bố Mẽo, nhanh nhưng không đọc link hình ảnh',
-                '`qwen-vl-plus` - Của bố khựa, chỉ nên dùng để đọc hình ảnh'
-            ]
-            await interaction.response.send_message('\n'.join(response))
-            return
         user_id = interaction.user.id
         self.AIChat_api_key_collection.update_one({'_id': user_id}, {'$set': {'model': model}}, upsert=True)
         if api_key is None: # chi thay doi model
@@ -66,17 +61,18 @@ class AIChatCommand(commands.Cog):
             return
         
         first_embed = Embed(description="\u200b",color=Color.purple())
+        first_embed.set_footer(text=f"{model}")
         response_message = await ctx.send(embed=first_embed)  # tin nhắn ban đầu
-        await asyncio.sleep(2)
+        await asyncio.sleep(1)
         #xu ly prompt
         prompt = self.parse_prompt(prompt)
         try:
             response_stream = self.to_the_moon(api_key=api_key, model=model, message=prompt)
-            await self.update_embed(response_message, response_stream, model=model)
+            await self.update_embed(response_message, response_stream, model)
             return
         except Exception as e:
             print(e)
-            await response_message.edit(content=f'{e}')
+            await response_message.edit(embed=Embed(description=f"{e}\n¯\_(ツ)_/¯\nHãy kiểm tra lại model/api key", color=Color.red()))
             return
 
     def parse_prompt(self, prompt: str):
